c1e9b4cae42ae6e32a96f4e85d6608d335474e89
louis <louissu024@gmail.com>
2024-09-21 20:14:44 +0800

    123

---
+++ Pytorch-CIFAR10/Pytorch-CIFAR10-main/main.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from tqdm.autonotebook import tqdm
+from models import *
+import numpy as np
+
+device = torch.device('cuda')
+print('Using device:', device)
+#print('GPU:', torch.cuda.get_device_name(0))
+
+#net = resnet().to(device)
+net = LeNet().to(device)
+#net = VGG().to(device)
+# net = GoogLeNet().to(device)
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+)
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=True)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+num_epochs = 7
+losses = []
+batches = len(trainloader)
+
+def train():
+   print("TRAINING")
+   for epoch in range(num_epochs):
+      progress = tqdm(enumerate(trainloader), desc="Loss: ", total=batches)
+      total_loss = 0
+      for i, (inputs, labels) in progress:
+         inputs, labels = inputs.to(device), labels.to(device)
+         optimizer.zero_grad()
+         output= net(inputs)
+         loss = criterion(output, labels)
+         
+         loss.backward()
+         optimizer.step()
+         
+         current_loss = loss.item()
+         total_loss += current_loss
+         progress.set_description("Loss: {:.4f}".format(total_loss/(i+1)))
+      losses.append(total_loss/batches)
+      print(f"Epoch {epoch+1}/{num_epochs}, Loss: {total_loss/batches}")
+
+   #torch.save(net, './save')
+
+
+def test():
+   total_correct = 0
+   total_images = 0
+   confusion_matrix = np.zeros([10,10], int)
+   with torch.no_grad():
+       for inputs, labels in testloader:
+           inputs, labels = inputs.to(device), labels.to(device)
+           outputs = net(inputs)
+           _, predicted = torch.max(outputs.data, 1)
+           total_images += labels.size(0)
+           total_correct += (predicted == labels).sum().item()
+           for i, l in enumerate(labels):
+               confusion_matrix[l.item(), predicted[i].item()] += 1 
+
+   model_accuracy = total_correct / total_images * 100
+   print('Model accuracy on {0} test images: {1:.2f}%'.format(total_images, model_accuracy))
+
+#net = torch.load("save")
+
+train()
+test()
---
+++ Pytorch-CIFAR10/Pytorch-CIFAR10-main/models/googlenet.py
@@ -0,0 +1,97 @@
+import torch.nn as nn
+class Inception(nn.Module):
+	 def __init__(self, in_planes, n1, n3_reduce, n3, n5_reduce, n5, pool_proj):
+			super(Inception, self).__init__()
+
+			self.block1 = nn.Sequential(
+						nn.Conv2d(in_planes, n1, kernel_size=1),
+						nn.BatchNorm2d(n1),
+						nn.ReLU(True),
+				 )
+
+			self.block2 = nn.Sequential(
+						nn.Conv2d(in_planes, n3_reduce, kernel_size=1),
+						nn.BatchNorm2d(n3_reduce),
+						nn.ReLU(True),
+
+						nn.Conv2d(n3_reduce, n3, kernel_size=3, padding=1),
+						nn.BatchNorm2d(n3),
+						nn.ReLU(True),
+				 )
+
+			self.block3 = nn.Sequential(
+						nn.Conv2d(in_planes, n5_reduce, kernel_size=1),
+						nn.BatchNorm2d(n5_reduce),
+						nn.ReLU(True),
+
+						nn.Conv2d(n5_reduce, n5, kernel_size=3, padding=1),
+						nn.BatchNorm2d(n5),
+						nn.ReLU(True),
+
+						nn.Conv2d(n5, n5, kernel_size=3, padding=1),
+						nn.BatchNorm2d(n5),
+						nn.ReLU(True),
+				 )
+
+			self.block4 = nn.Sequential(
+						nn.MaxPool2d(3, stride=1, padding=1),
+						nn.Conv2d(in_planes, pool_proj, kernel_size=1),
+						nn.BatchNorm2d(pool_proj),
+						nn.ReLU(True),
+				 )
+
+	 def forward(self, x):
+			y1 = self.block1(x)
+			y2 = self.block2(x)
+			y3 = self.block3(x)
+			y4 = self.block4(x)
+
+			return torch.cat([y1,y2,y3,y4], 1)
+
+
+
+class GoogLeNet(nn.Module):
+	 def __init__(self):
+			super(GoogLeNet, self).__init__()
+
+			self.pre_layers = nn.Sequential(
+						nn.Conv2d(3, 192, kernel_size=3, padding=1),
+						nn.BatchNorm2d(192),
+						nn.ReLU(True),
+				 )
+
+			self.a3 = Inception(192,  64,  96, 128, 16, 32, 32)
+			self.b3 = Inception(256, 128, 128, 192, 32, 96, 64)
+		
+			self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
+
+			self.a4 = Inception(480, 192,  96, 208, 16,  48,  64)
+			self.b4 = Inception(512, 160, 112, 224, 24,  64,  64)
+			self.c4 = Inception(512, 128, 128, 256, 24,  64,  64)
+			self.d4 = Inception(512, 112, 144, 288, 32,  64,  64)
+			self.e4 = Inception(528, 256, 160, 320, 32, 128, 128)
+
+			self.a5 = Inception(832, 256, 160, 320, 32, 128, 128)
+			self.b5 = Inception(832, 384, 192, 384, 48, 128, 128)
+
+			self.avg_pool = nn.AvgPool2d(8, stride=1)
+			self.linear = nn.Linear(1024, 10)
+
+	 def forward(self, x):
+			out = self.pre_layers(x)
+			out = self.a3(out)
+			out = self.b3(out)
+			out = self.maxpool(out)
+			out = self.a4(out)
+			out = self.b4(out)
+			out = self.c4(out)
+			out = self.d4(out)
+			out = self.e4(out)
+			out = self.maxpool(out)
+			out = self.a5(out)
+			out = self.b5(out)
+			out = self.avgpool(out)
+			out = out.view(out.size(0), -1)
+			out = self.linear(out)
+		
+			return out
